41894fb041988bbe27b7fa3f06e6d60e1a63a208
YiRan <yiranli2005@gmail.com>
2026-01-30 23:50:30 +0000

    added help for run

--- src/interpreter.c
+++ src/interpreter.c
@@ -91,7 +91,8 @@ quit			Exits / terminates the shell with “Bye!”\n \
 set VAR STRING		Assigns a value to shell memory\n \
 print VAR		Displays the STRING assigned to VAR\n \
 source SCRIPT.TXT	Executes the file SCRIPT.TXT\n \
-echo STRING/VAR        Print the STRING or the VAR value\n ";
+echo STRING/VAR	Prints the STRING or the VAR value\n \
+run COMMAND ARGS	Runs an external command using fork-exec-wait\n";
     printf("%s\n", help_string);
     return 0;
 }
