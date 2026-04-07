8f4c78a8eeb56c1e0ea644cb616271962abbed8e
Fiona <fionazhaofiona@gmail.com>
2026-04-07 22:24:46 +0000

    Fix: variable declaration

--- src/interpreter.c
+++ src/interpreter.c
@@ -543,10 +543,10 @@ int my_exec(char *args[], int args_size, bool MT) {
 	struct PCB *pcb_already = program_already_scheduled(q, args[n]);
 	struct PCB *pcb = NULL;
 	if (pcb_already) {
-	    struct PCB *pcb = create_procress_already(pcb_already);
+	    pcb = create_procress_already(pcb_already);
 	}
 	else {
-            struct PCB *pcb = create_process(args[n]);
+            pcb = create_process(args[n]);
 	}
         if (!pcb) {
             printf("Failed to create process\n");
